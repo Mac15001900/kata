2d4719228fa7dc04c85a4fb82d98d6ce1ca18150
Mac15001900 <mac15001900@gmail.com>
2026-04-21 20:43:01 +0200

    Created very basic biome data

--- data/biomes.js
+++ data/biomes.js
@@ -2,4 +2,73 @@ import { BIOME } from "../js/objects";
 
 export const BIOME_DATA = {}
 
-// BIOME_DATA[BIOME.RED] = 
+BIOME_DATA[BIOME.RED] = {
+    id: BIOME.RED,
+    heart: ':heart:',
+    mapString: "♣♣♣♣♣♣",
+    lookDescription: `TODO są tu duże drzewa TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
+
+BIOME_DATA[BIOME.ORANGE] = {
+    id: BIOME.ORANGE,
+    heart: ':orange_heart:',
+    mapString: "-◠-◠-◠-",
+    lookDescription: `TODO jest tu dużo miodu TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
+
+
+BIOME_DATA[BIOME.YELLOW] = {
+    id: BIOME.YELLOW,
+    heart: ':yellow_heart:',
+    mapString: "▁▂▂▃▂▂▁",
+    lookDescription: `TODO jest to duży marmurowy blok ze złotymi zdobieniami TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
+
+BIOME_DATA[BIOME.GREEN] = {
+    id: BIOME.GREEN,
+    heart: ':green_heart:',
+    mapString: "└—┘~└—┘",
+    lookDescription: `TODO bagno z gigantycznymi liliami wodnymi TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
+
+BIOME_DATA[BIOME.CYAN] = {
+    id: BIOME.CYAN,
+    heart: ':light_blue_h:eart',
+    mapString: "∆_∇∆∇_∆",
+    lookDescription: `TODO latające szkło i zakrzywiona czasoprzestrzeń TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
+
+BIOME_DATA[BIOME.BLUE] = {
+    id: BIOME.BLUE,
+    heart: ':blue_heart:',
+    mapString: "_I__I_",
+    lookDescription: `TODO niebieska trawa z metalowymi pilarami TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
+
+BIOME_DATA[BIOME.VIOLET] = {
+    id: BIOME.VIOLET,
+    heart: ':purple_heart:',
+    mapString: "✧･ﾟ: *✧･ﾟ:*",
+    lookDescription: `TODO niezdefiniowane magiczne coś TODO`,
+    harvestLoot: [],
+    searchLoot: [],
+    digLoot: [],
+}
